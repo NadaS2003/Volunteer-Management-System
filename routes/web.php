cbc8aa61986181915d5e83975b9e85bb8bf058e3
Nada Salama <nadaramadan1512003@gmail.com>
2026-07-25 22:24:03 +0300

    Update Route to install passport

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\PlaceController;
 use App\Http\Controllers\TaskController;
 use App\Http\Controllers\VolunteerController;
+use Laravel\Passport\Client;
 use Illuminate\Support\Facades\Route;
 
 /*
